0bc67ecd66a50dda2c37a12668e1dc8b3d688ef1
Jeetrathorer <132336193+Jeetrathorer@users.noreply.github.com>
2024-02-04 22:39:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "@jeetrathores",
+    "https://github.com/Jeetrathorer/-e-x-s-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
