215f2971818ed8d9432c36806dc2813ecbfda7bb
Jeetrathorer <132336193+Jeetrathorer@users.noreply.github.com>
2024-02-05 13:12:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dphouseforyou")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/jaymaakali76")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", true))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "False")
 AUTO_SUGGESTION_TIME = int(
     getenv("AUTO_SUGGESTION_TIME", "500"))
